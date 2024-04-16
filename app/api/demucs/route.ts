1e0be61f43f879cbf2700c8d4d0cd58c2019f868
Chris Abdo <chris.j.abdo@gmail.com>
2024-04-15 22:16:10 -0400

    default replicate init

--- app/api/demucs/route.ts
+++ app/api/demucs/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN,
-});
+const replicate = new Replicate();
 
 export async function POST(request: Request) {
   if (!request) {
