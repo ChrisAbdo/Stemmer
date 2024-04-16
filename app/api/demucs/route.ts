3d71fed5324b907453d0d899abcc9beabce4759e
Chris Abdo <chris.j.abdo@gmail.com>
2024-04-15 22:24:33 -0400

    hardcode again

--- app/api/demucs/route.ts
+++ app/api/demucs/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
 const replicate = new Replicate({
-  auth: process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN,
+  // auth: process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN,
+  auth: "r8_MLn1P4nAzrzHKhGZTtGKn1X9iIFRN2n2en9FF",
 });
 
 export async function POST(request: Request) {
